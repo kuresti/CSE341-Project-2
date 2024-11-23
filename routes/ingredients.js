98657f9b7c76bdd0eeb51c9b6c479ab772d238c7
kuresti <55415865+kuresti@users.noreply.github.com>
2024-11-23 12:33:27 -0700

    Create ingredients.js
    -Added GET all route
    -Added GET single route
    -Added POST route
    -Added PUT route
    -Added DELETE route

---
+++ routes/ingredients.js
@@ -0,0 +1,41 @@
+/* *******************************
+ * Required Resources
+ * *******************************/
+const express = require('express');
+const router = express.Router();
+const ingredientsCont = require('../controllers/ingredients');
+const validate = require('../validation/validate');
+const { handleErrors } = require('../utilities/validation');
+
+/* *******************************
+ * Get Routes
+ * *******************************/
+router.get('/', ingredientsCont.getAll);
+
+router.get('/:id', handleErrors(ingredientsCont.getSingle));
+
+/* *******************************
+ * POST Routes
+ * *******************************/
+router.post(
+    '/', 
+    validate.ingredientsRules(),
+    validate.validateIngredient,
+   handleErrors(ingredientsCont.createNewIngredient)
+)
+
+/* *******************************
+ * PUT Routes
+ * *******************************/
+router.put('/:id', 
+    validate.ingredientsRules(),
+    validate.validateIngredient,
+    handleErrors(ingredientsCont.updateIngredient)
+)
+
+/* *******************************
+ * DELETE Routes
+ * *******************************/
+router.delete('/:id', handleErrors(ingredientsCont.deleteIngredient));
+
+module.exports = router;
