681195649437e423946818e2b8e63e508601a6c4
kuresti <55415865+kuresti@users.noreply.github.com>
2024-11-30 22:18:45 -0700

    Update index.js
    -Added login and logout routes

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,29 @@
  * ******************************/
 const express = require('express');
 const router = require('express').Router();
+const passport = require('passport')
 
 
 /* ******************************
  *  Routes
  * ******************************/
-router.get('/', (req, res,) => {
-    //swagger.tags=['Hello world']
-    res.send('Hello World');
-})
+// router.get('/', (req, res,) => {
+//     //swagger.tags=['Hello world']
+//     res.send('Hello World');
+// })
+
+// Login route
+router.get('/login', passport.authenticate('github'), (req, res) => {});
+
+// Logout route
+router.get('/logout', function(req, res, next) {
+    req.logout(function(err) {
+        if (err) {
+            return next(err);
+        }
+        res.redirect('/');
+    });
+});
 
 // Swagger route
 router.use('/', require('./swagger'));
