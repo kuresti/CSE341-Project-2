44a845c126fad49e6a55b431720f7c26b24b004e
kuresti <55415865+kuresti@users.noreply.github.com>
2024-11-30 22:22:12 -0700

    Update nutrition.js
    -reconfigured error Handling
    -Added isAuthenticated

--- routes/nutrition.js
+++ routes/nutrition.js
@@ -6,36 +6,42 @@ const router = express.Router();
 const nutritionCont = require('../controllers/nutrition');
 const validate = require('../validation/validate');
 const { handleErrors } = require('../utilities/validation');
+const { isAuthenticated } = require('../utilities/authenticate');
 
 /* *******************************
  * Get Routes
  * *******************************/
-router.get('/', handleErrors(nutritionCont.getAll));
+router.get('/', nutritionCont.getAll);
 
-router.get('/:id', handleErrors(nutritionCont.getSingle));
+router.get('/:id', nutritionCont.getSingle);
 
 /* *******************************
  * POST Routes
  * *******************************/
 router.post(
     '/', 
+    isAuthenticated,
     validate.nutritionRules(),
     validate.validateNutrition,
-    handleErrors(nutritionCont.createNewNutrition)
+    nutritionCont.createNewNutrition
 )
 
 /* *******************************
  * PUT Routes
  * *******************************/
 router.put('/:id', 
+    isAuthenticated,
     validate.nutritionRules(),
     validate.validateNutrition,
-    handleErrors(nutritionCont.updateNutrition)
+    nutritionCont.updateNutrition
 )
 
 /* *******************************
  * DELETE Routes
  * *******************************/
-router.delete('/:id', handleErrors(nutritionCont.deleteNutrition));
+router.delete('/:id', 
+    isAuthenticated,
+    nutritionCont.deleteNutrition);
 
+router.use(handleErrors)    
 module.exports = router;
